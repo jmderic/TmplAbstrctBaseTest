23b7effc85e332754153ca48735c1810ca71ee27
Mark Deric <codesmith@dericnet.com>
2020-03-18 16:24:05 -0700

    Create a quick start project for C/C++ tests

---
+++ main.c
@@ -0,0 +1,9 @@
+// Quick Start file for testing ideas/snippets
+
+//#include <stdio.h>
+
+//int main (int argc, char *argv[])
+int main ()
+{
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,8 @@
+// Quick Start file for testing ideas/snippets
+
+//#include <iostream>
+
+int main (int /*argc*/, char */*argv*/[])
+{
+    return 0;
+}
